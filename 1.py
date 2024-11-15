03aa8a88f9ee0ec29cc716391c9f225c68e3b344
dtretenichenko <dtretenichenko@kse.org.ua>
2024-11-15 15:46:15 +0200

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,6 @@
-print('Hello World')
+with open('Olympic Athletes - athlete_events.tsv', "rt") as file:
+    next(file)
+    for line in file:
+        line = line[:-1]
+        split = line.split('\t')
+        print(split)
