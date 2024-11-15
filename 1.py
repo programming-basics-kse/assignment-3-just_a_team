936e306d4240ca8be3f122b5f4f0aadf3425b804
dtretenichenko <dtretenichenko@kse.org.ua>
2024-11-15 16:42:03 +0200

    1

--- 1.py
+++ 1.py
@@ -1,6 +1,25 @@
+import argparse
 with open('Olympic Athletes - athlete_events.tsv', "rt") as file:
     next(file)
     for line in file:
         line = line[:-1]
         split = line.split('\t')
         print(split)
+        ID = split[0]
+        name = split[1]
+        sex = split[2]
+        age = split[3]
+        height = split[4]
+        weight = split[5]
+        team = split[6]
+        NOC = split[7]
+        games = split[8]
+        year = split[9]
+        season = split[10]
+        city = split[11]
+        sport = split[12]
+        event = split[13]
+        medal = split[14]
+
+parser = argparse.ArgumentParser(description='Olympic athlets for last 120 years.')
+parser.add_argument('')
